51a15bb2a3ba2167e02b35bd9214bdd35b6ef665
MadinaKon <141622998+MadinaKon@users.noreply.github.com>
2023-08-17 20:40:27 +0200

    separate js and link js in HTML

---
+++ index.js
@@ -0,0 +1,24 @@
+function toggleAnswer(answerId, buttonId) {
+  const answer = document.getElementById(answerId);
+  const button = document.getElementById(buttonId);
+
+  if (answer.style.display === "none" || button.innerHTML === "Show answer") {
+    button.innerHTML = "Hide answer";
+    answer.style.display = "block";
+  } else {
+    button.innerHTML = "Show answer";
+    answer.style.display = "none";
+  }
+}
+function toggleAnswer(answerId, buttonId) {
+  const answer = document.getElementById(answerId);
+  const button = document.getElementById(buttonId);
+
+  if (answer.style.display === "none" || button.innerHTML === "Show answer") {
+    button.innerHTML = "Hide answer";
+    answer.style.display = "block";
+  } else {
+    button.innerHTML = "Show answer";
+    answer.style.display = "none";
+  }
+}
